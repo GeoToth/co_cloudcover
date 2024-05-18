5f19626e7275206ce8a9dc71078155f46506c6f1
Christopher Toth <107810175+GeoToth@users.noreply.github.com>
2024-05-18 09:32:57 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='days of cloud cover from MODIS',
+    author='Chris_Toth',
+    license='MIT',
+)
